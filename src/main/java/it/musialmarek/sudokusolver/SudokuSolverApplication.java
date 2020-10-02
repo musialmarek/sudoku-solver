28a7610aaa5bfbdb81f4fe0524ba175eb30baa61
Marek Musiał <mmusialmarek@gmail.com>
2020-10-02 11:16:35 +0200

    Initila commit

---
+++ src/main/java/it/musialmarek/sudokusolver/SudokuSolverApplication.java
@@ -0,0 +1,13 @@
+package it.musialmarek.sudokusolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/musialmarek/sudokusolver/SudokuSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package it.musialmarek.sudokusolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
