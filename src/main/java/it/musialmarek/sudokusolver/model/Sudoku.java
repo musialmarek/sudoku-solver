8f099ffa7c7d034dbef3ca72297532888247c270
Marek Musiał <mmusialmarek@gmail.com>
2020-10-02 15:36:00 +0200

    created setter for Sudoku's array and method to fill values of Sudoku's columns

--- src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
+++ src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
@@ -2,11 +2,30 @@ package it.musialmarek.sudokusolver.model;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
 
 @Getter
+@NoArgsConstructor
 public class Sudoku {
-    private Integer[][] tab = new Integer[9][9];
+    private Integer[][] array = new Integer[9][9];
     private Integer[][] cols = new Integer[9][];
     private Integer[][] rows = new Integer[9][];
     private Integer[][] sections = new Integer[9][];
+
+    public void setArray(Integer[][] array) {
+        this.array = array;
+    }
+
+    public void fillCols() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Integer element = array[j][i];
+                if (element != null) {
+                    cols[i] = ArrayUtils.add(cols[i], element);
+                }
+            }
+        }
+    }
+
 }
