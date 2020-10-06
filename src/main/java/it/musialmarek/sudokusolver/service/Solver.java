f546f94c9b0bd9aa95a6e7634c34b7a470cdf9a0
Marek Musiał <mmusialmarek@gmail.com>
2020-10-06 08:29:02 +0200

    added methods to fill empty fields with algorithm basing on numbers which have only one possible place in a sector (row, column (col) or section)

--- src/main/java/it/musialmarek/sudokusolver/service/Solver.java
+++ src/main/java/it/musialmarek/sudokusolver/service/Solver.java
@@ -4,7 +4,6 @@ import it.musialmarek.sudokusolver.model.Sudoku;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,21 +12,82 @@ import java.util.Set;
 public class Solver {
     public static Sudoku solveSudoku(Sudoku sudoku) {
 
-        Integer numberOfNullsStart = 1;
-        Integer numberOfNullEnd = 0;
+        int numberOfNullsStart = 1;
+        int numberOfNullEnd = 0;
+
         while (numberOfNullEnd < numberOfNullsStart) {
             numberOfNullsStart = getNumberOfNulls(sudoku);
-            fillEmptyBySingle(sudoku);
+            fillEmptiesBySingle(sudoku);
+            fillEmptiesByOnlys(sudoku);
             numberOfNullEnd = getNumberOfNulls(sudoku);
         }
         return sudoku;
     }
 
-    private static void fillEmptyBySingle(Sudoku sudoku) {
+    private static void fillEmptiesByOnlys(Sudoku sudoku) {
+        fillEmptiesByRowOnlys(sudoku);
+        fillEmptiesByColOnlys(sudoku);
+        fillEmptiesBySectionOnlys(sudoku);
+    }
+
+    private static void fillEmptiesBySectionOnlys(Sudoku sudoku) {
+        for (int i = 0; i < sudoku.getArray().length; i++) {
+            Integer[][][] possibilities = getPossibilities(sudoku);
+            Integer[] onlys = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
+            for (int j = 0; j < sudoku.getArray().length; j++) {
+                onlys = ArrayUtils.removeElements(onlys, possibilities[j / 3 + 3 * (i / 3)][j % 3 + 3 * (i % 3)]);
+            }
+            for (int j = 0; j < sudoku.getArray().length; j++) {
+                for (Integer only : onlys) {
+                    int x = j / 3 + 3 * (i / 3);
+                    int y = j % 3 + 3 * (i % 3);
+                    if (sudoku.getArray()[x][y] == null && ArrayUtils.contains(possibilities[x][y], only)) {
+                        sudoku.getArray()[x][y] = only;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fillEmptiesByColOnlys(Sudoku sudoku) {
+        for (int col = 0; col < sudoku.getArray().length; col++) {
+            Integer[][][] possibilities = getPossibilities(sudoku);
+            Integer[] onlys = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
+            for (int row = 0; row < sudoku.getArray().length; row++) {
+                onlys = ArrayUtils.removeElements(onlys, possibilities[row][col]);
+            }
+            for (int row = 0; row < sudoku.getArray().length; row++) {
+                for (Integer only : onlys) {
+                    if (sudoku.getArray()[row][col] == null && ArrayUtils.contains(possibilities[row][col], only)) {
+                        sudoku.getArray()[row][col] = only;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fillEmptiesByRowOnlys(Sudoku sudoku) {
+        for (int row = 0; row < sudoku.getArray().length; row++) {
+            Integer[][][] possibilities = getPossibilities(sudoku);
+            Integer[] onlys = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9};
+            for (int col = 0; col < sudoku.getArray().length; col++) {
+                onlys = ArrayUtils.removeElements(onlys, possibilities[row][col]);
+            }
+            for (int col = 0; col < sudoku.getArray().length; col++) {
+                for (Integer only : onlys) {
+                    if (sudoku.getArray()[row][col] == null && ArrayUtils.contains(possibilities[row][col], only)) {
+                        sudoku.getArray()[row][col] = only;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void fillEmptiesBySingle(Sudoku sudoku) {
         Integer[][][] possibilities = getPossibilities(sudoku);
         for (int i = 0; i < sudoku.getArray().length; i++) {
             for (int j = 0; j < sudoku.getArray()[i].length; j++) {
-                if (possibilities[i][j] != null && possibilities[i][j].length == 1) {
+                if (possibilities[i][j] != null && possibilities[i][j].length == 1 && sudoku.getArray()[i][j] == null) {
                     sudoku.getArray()[i][j] = possibilities[i][j][0];
                 }
             }
@@ -59,6 +119,8 @@ public class Solver {
                     notAvailableSet.addAll(List.of(sudoku.getSections()[calculateSectionIndex(i, j)]));
                     Integer[] notAvailableNumbers = notAvailableSet.toArray(new Integer[0]);
                     possibilities[i][j] = ArrayUtils.removeElements(set, notAvailableNumbers);
+                } else {
+                    possibilities[i][j] = ArrayUtils.add(new Integer[0], sudoku.getArray()[i][j]);
                 }
             }
         }
