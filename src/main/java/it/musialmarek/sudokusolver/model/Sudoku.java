d94958fd5b573416e6f0106e9e6a2e4dc06c4ea7
Marek Musiał <mmusialmarek@gmail.com>
2020-10-29 19:26:29 +0100

    extracted getters and setters to lombok and overrode toString

--- src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
+++ src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
@@ -1,21 +1,36 @@
 package it.musialmarek.sudokusolver.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
 public class Sudoku {
     private Integer[][] array = new Integer[9][9];
 
-    public Sudoku(Integer[][] array) {
-        this.array = array;
-    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (int i = 0; i < array.length; i++) {
+            if(i%3==0){
+                sb.append("---------------------\n");
+            }
+            for (int j = 0; j < array[i].length; j++) {
+                if(j%3==0){
+                    sb.append("|");
+                }
+                sb.append(array[i][j]).append(",");
 
-    public Integer[][] getArray() {
-        return array;
-    }
-
-    public void setArray(Integer[][] array) {
-        this.array = array;
+            }
+            sb.append("|\n");
+        }
+        return sb.toString();
     }
 }
