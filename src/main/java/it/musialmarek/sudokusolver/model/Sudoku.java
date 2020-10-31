8f378fc08abc549abe0d417411a5a3f256d34974
Marek Musiał <mmusialmarek@gmail.com>
2020-10-31 08:30:08 +0100

    changed class Sudoku to be resizable

--- src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
+++ src/main/java/it/musialmarek/sudokusolver/model/Sudoku.java
@@ -17,20 +17,31 @@ public class Sudoku {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        int size = this.getSize();
         sb.append("\n");
-        for (int i = 0; i < array.length; i++) {
-            if(i%3==0){
+        for (int row = 0; row < array.length; row++) {
+            if (row % size == 0) {
                 sb.append("---------------------\n");
             }
-            for (int j = 0; j < array[i].length; j++) {
-                if(j%3==0){
+            for (int col = 0; col < array[row].length; col++) {
+                if (col % size == 0) {
                     sb.append("|");
                 }
-                sb.append(array[i][j]).append(",");
+                sb.append(array[row][col]).append(",");
 
             }
             sb.append("|\n");
         }
         return sb.toString();
+    }
+
+    public Sudoku setSize(int size) {
+        int length = size * size;
+        this.array = new Integer[length][length];
+        return this;
+    }
+
+    public int getSize() {
+        return (int) Math.pow(this.array.length, 0.5);
     }
 }
