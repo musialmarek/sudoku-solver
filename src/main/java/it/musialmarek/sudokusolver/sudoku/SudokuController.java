4714f1587d425ac5a91b2710a6c54f4eeb6d0ca5
Marek Musiał <mmusialmarek@gmail.com>
2020-10-31 11:52:13 +0100

    returned to fill sudoku array with HttpServletRequest

--- src/main/java/it/musialmarek/sudokusolver/sudoku/SudokuController.java
+++ src/main/java/it/musialmarek/sudokusolver/sudoku/SudokuController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,8 +26,10 @@ public class SudokuController {
     }
 
     @GetMapping()
-    public String showEmptyGrid(Model model) {
-        int size = 3;
+    public String showEmptyGrid(Model model, @RequestParam(name = "size", required = false) Integer size) {
+        if (size == null) {
+            size = 3;
+        }
         Sudoku sudoku = new Sudoku().setSize(size);
         log.debug("added empty sudoku {}", sudoku.toString());
         model.addAttribute("sudoku", sudoku);
@@ -35,17 +38,37 @@ public class SudokuController {
     }
 
     @PostMapping()
-    public String solveSudoku(HttpServletRequest request, Model model, Sudoku sudoku) {
+    public String solveSudoku(HttpServletRequest request, Model model) {
+        log.debug("solving sudoku");
+        int size = Integer.parseInt(request.getParameter("size"));
+        Sudoku sudoku = getSudoku(request, size);
         log.debug(sudoku.toString());
         if (Solver.isSudokuCorrect(sudoku)) {
             Sudoku solvedSudoku = Solver.solveSudoku(sudoku);
-            model.addAttribute("solvedsudoku", solvedSudoku.getArray());
+            model.addAttribute("solvedsudoku", solvedSudoku);
         } else {
             model.addAttribute("message", "INCORRECT SUDOKU");
         }
         model.addAttribute("sudoku", sudoku);
-        int size = 3;
         model.addAttribute("size", size);
         return "sudoku-solver";
+    }
+
+    private Sudoku getSudoku(HttpServletRequest request, int size) {
+        Sudoku sudoku = new Sudoku().setSize(size);
+        Integer[][] sudokuArray = sudoku.getArray();
+        for (int row = 0; row < sudokuArray.length; row++) {
+            for (int col = 0; col < sudokuArray[row].length; col++) {
+                String cell = "sudoku" + row + "," + col;
+                String value = request.getParameter(cell);
+                if (value.isEmpty()) {
+                    sudokuArray[row][col] = null;
+                } else {
+                    sudokuArray[row][col] = Integer.parseInt(value);
+                }
+            }
+        }
+        sudoku.setArray(sudokuArray);
+        return sudoku;
     }
 }
