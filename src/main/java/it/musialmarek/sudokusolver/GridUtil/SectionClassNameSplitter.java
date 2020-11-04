b8df68a9c2c4f46e7f00fa1aa808754df2f89e3c
Marek Musiał <mmusialmarek@gmail.com>
2020-11-04 12:11:58 +0100

    renamed SectionSplitter

--- src/main/java/it/musialmarek/sudokusolver/GridUtil/SectionClassNameSplitter.java
+++ src/main/java/it/musialmarek/sudokusolver/GridUtil/SectionClassNameSplitter.java
@@ -3,7 +3,7 @@ package it.musialmarek.sudokusolver.GridUtil;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SectionSplitter {
+public class SectionClassNameSplitter {
     public static boolean assignCellToSection(int row, int col, int size) {
         int section = row - row % size + (col - col % size) / size;
         return (section / size + section % size) % 2 == 0;
