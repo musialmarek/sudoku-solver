8abc3738685d8ef00d321c1d1e7e8d6b74d74917
Marek Musiał <mmusialmarek@gmail.com>
2020-11-11 19:49:58 +0100

    parametrized test of SudokuTest

--- src/test/java/it/musialmarek/sudokusolver/model/SudokuTest.java
+++ src/test/java/it/musialmarek/sudokusolver/model/SudokuTest.java
@@ -1,70 +1,79 @@
 package it.musialmarek.sudokusolver.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SudokuTest {
 
-    @Test
-    void shouldReturn3WhenArrayLength9() {
+    @ParameterizedTest(name = "should return size {0} when array length {1}")
+    @CsvFileSource(resources = "/sizes-and-lengths.csv", numLinesToSkip = 1)
+    void shouldReturnSizeByArrayLength(int size, int length) {
         //Given
-        Sudoku sudoku = buildSudokuByArrayLength(9);
+        Sudoku sudoku = buildSudokuByArrayLength(length);
         //When
-        int size = sudoku.getSize();
+        int sudokuSize = sudoku.getSize();
         //Then
-        assertThat(size).isEqualTo(3);
+        assertThat(sudokuSize).isEqualTo(size);
     }
 
-    @Test
-    void shouldReturn4WhenArrayLength16() {
-        //Given
-        Sudoku sudoku = buildSudokuByArrayLength(16);
-        //When
-        int size = sudoku.getSize();
-        //Then
-        assertThat(size).isEqualTo(4);
-    }
-
-    @Test
-    void shouldReturn2WhenArrayLength4() {
-        //Given
-        Sudoku sudoku = buildSudokuByArrayLength(4);
-        //When
-        int size = sudoku.getSize();
-        //Then
-        assertThat(size).isEqualTo(2);
-    }
-
-    @Test
-    void shouldReturnSudokuWithArrayLength4WhenSetSize2() {
+    @ParameterizedTest(name = "should set array length {1} when set size {0}")
+    @CsvFileSource(resources = "/sizes-and-lengths.csv", numLinesToSkip = 1)
+    void shouldSetArrayLengthWhenSudokuSetSize(int size, int length) {
         //Given
         Sudoku sudoku = new Sudoku();
         //When
-        sudoku.setSize(2);
+        sudoku.setSize(size);
         //Then
-        assertThat(sudoku.getArray().length).isEqualTo(4);
+        assertThat(sudoku.getArray().length).isEqualTo(length);
     }
 
-    @Test
-    void shouldReturnSudokuWithArrayLength9WhenSetSize3() {
-        //Given
-        Sudoku sudoku = new Sudoku();
-        //When
-        sudoku.setSize(3);
-        //Then
-        assertThat(sudoku.getArray().length).isEqualTo(9);
-    }
-
-    @Test
-    void shouldReturnSudokuWithArrayLength16WhenSetSize4() {
-        //Given
-        Sudoku sudoku = new Sudoku();
-        //When
-        sudoku.setSize(4);
-        //Then
-        assertThat(sudoku.getArray().length).isEqualTo(16);
-    }
+//    @Test
+//    void shouldReturn4WhenArrayLength16() {
+//        //Given
+//        Sudoku sudoku = buildSudokuByArrayLength(16);
+//        //When
+//        int size = sudoku.getSize();
+//        //Then
+//        assertThat(size).isEqualTo(4);
+//    }
+//
+//    @Test
+//    void shouldReturn2WhenArrayLength4() {
+//        //Given
+//        Sudoku sudoku = buildSudokuByArrayLength(4);
+//        //When
+//        int size = sudoku.getSize();
+//        //Then
+//        assertThat(size).isEqualTo(2);
+//    }
+//
+//    @Test
+//    void shouldReturnSudokuWithArrayLength4WhenSetSize2() {
+//
+//    }
+//
+//    @Test
+//    void shouldReturnSudokuWithArrayLength9WhenSetSize3() {
+//        //Given
+//        Sudoku sudoku = new Sudoku();
+//        //When
+//        sudoku.setSize(3);
+//        //Then
+//        assertThat(sudoku.getArray().length).isEqualTo(9);
+//    }
+//
+//    @Test
+//    void shouldReturnSudokuWithArrayLength16WhenSetSize4() {
+//        //Given
+//        Sudoku sudoku = new Sudoku();
+//        //When
+//        sudoku.setSize(4);
+//        //Then
+//        assertThat(sudoku.getArray().length).isEqualTo(16);
+//    }
 
     private Sudoku buildSudokuByArrayLength(int arrayLength) {
         return new Sudoku(new Integer[arrayLength][arrayLength]);
